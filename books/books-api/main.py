2d77f7cf11ccd092ca2c5dff14dbe8d9c3527ee6
Kathryn Hodge <blondiebytes@gmail.com>
2023-03-16 13:29:09 -0700

    Update main.py

--- books/books-api/main.py
+++ books/books-api/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from . import schemas
+from . import database
 
 app = FastAPI()
 
@@ -9,6 +10,12 @@ def get_root():
 
 @app.post("/book/")
 def create_book(request: schemas.BookAuthorPayload):
+	database.add_book(convert_into_book_db_model(request.book), convert_into_author_db_model(request.author))
 	return "New book added " + request.book.title + " " + str(request.book.number_of_pages) \
 	+ " New author added " + request.author.first_name + " " + request.author.last_name
 
+def convert_into_book_db_model(book: schemas.Book):
+	return database.Book(title=book.title, number_of_pages=book.number_of_pages)
+
+def convert_into_author_db_model(author: schemas.Author):
+	return database.Author(first_name=author.first_name, last_name=author.last_name)
