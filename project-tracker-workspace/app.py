f28b3a8ed7bf3141e8a41e230d5dfd2381fce4c0
Kathryn Hodge <blondiebytes@gmail.com>
2023-03-17 14:13:06 -0700

    Implement 03_09

--- project-tracker-workspace/app.py
+++ project-tracker-workspace/app.py
@@ -1,11 +1,22 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://postgres:password@localhost/project_tracker"
+app.config["SECRET_KEY"] = b'\xa6\xcbo-Z\xf6\xeb\xce\xb0\xbc\xf9"\xba\xad\xed\xe2\xa4L\x14\x1a\xde\x8eg@'
+db = SQLAlchemy(app)
+
+class Project(db.Model):
+	__tablename__ = 'projects'
+	project_id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.String(length=50))
+
+
 
 #Define a route
 @app.route("/")
 def show_projects():
-	return render_template("index.html")
+	return render_template("index.html", projects=Project.query.all())
 
 @app.route("/project/<project_id>")
 def show_tasks(project_id):
