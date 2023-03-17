356b3fa593df242dc684a8bff2b4be66d7e113f6
Kathryn Hodge <blondiebytes@gmail.com>
2023-03-17 10:35:40 -0700

    Add routes

--- project-tracker-workspace/app.py
+++ project-tracker-workspace/app.py
@@ -7,4 +7,19 @@ app = Flask(__name__)
 def show_projects():
 	return render_template("index.html")
 
+@app.route("/project/<project_id>")
+def show_tasks(project_id):
+	return render_template("project-tasks.html", project_id=project_id)
+
+@app.route("/add/project", methods=['POST'])
+def add_project():
+	# TODO: Add project
+	return "Project added successfully"
+
+@app.route("/add/task/<project_id>", methods=['POST'])
+def add_task(project_id):
+	# TODO: Add task
+	return "Task added successfully"
+
+
 app.run(debug=True, host="127.0.0.1", port=3000)
